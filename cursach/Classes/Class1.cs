ba5d77727c87d446080c134e3733ede4566476d6
Ilia <korolenya.ilya2017@gmail.com>
2024-05-02 20:32:20 +0300

    test

--- cursach/Classes/Class1.cs
+++ cursach/Classes/Class1.cs
@@ -2,6 +2,7 @@
 {
     public class Class1
     {
+        public int a { get;set;}
 
     }
 }
--- cursach/TypingTutor/Form1.cs
+++ cursach/TypingTutor/Form1.cs
@@ -1,3 +1,4 @@
+using Classes;
 namespace TypingTutor
 {
     public partial class Form1 : Form
@@ -5,6 +6,13 @@ namespace TypingTutor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 a = new Class1();
+            a.a = 1;
+            Console.WriteLine($"{a.a}");
         }
     }
 }
