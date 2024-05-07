dfaed810fa452b06166a36648c6304bcd03b2fd3
DenShersh <georggnutiy@gmail.com>
2024-05-07 19:15:19 +0200

    Added abstarct base Stats class

---
+++ cursach/Classes/Stats.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public abstract class Stats
+    {
+    }
+}
