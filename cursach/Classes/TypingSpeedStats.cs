3a1810c24567c2d304f116e4f2334e1c8e263c93
DenShersh <georggnutiy@gmail.com>
2024-05-09 17:55:13 +0200

    Added a TypingSpeed class with some workground

---
+++ cursach/Classes/TypingSpeedStats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class TypingSpeedStats : Stats
+    {
+        protected float TypingSpeed { get; set; }    // words per minute
+        private Stopwatch stopwatch;
+        const float SecondsInAMinute = 60.0f;
+
+        public TypingSpeedStats() : base("w/m")
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+        }
+
+        public override void CalculateStat(uint NumberOfCorrectlyTypedWords)
+        {
+            stopwatch.Stop();
+            TypingSpeed = ((float)NumberOfCorrectlyTypedWords / stopwatch.ElapsedMilliseconds) * 1000.0f * SecondsInAMinute;
+            stopwatch.Start();
+        }
+    }
+}
