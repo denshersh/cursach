ba3450da903cc0301f970640690f1fc857297efe
DenShersh <georggnutiy@gmail.com>
2024-05-09 17:55:28 +0200

    Added a WordAccuracy class with some workground

---
+++ cursach/Classes/WordAccuracyStats.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class WordAccuracyStats : CharacterAccuracyStats
+    {
+
+        public WordAccuracyStats() : base(" % words accuracy") { }
+
+        public override float GetUnitAccuracy() { return UnitAccuracy; }
+
+        public override void CalculateStat(uint CorrectTypedChars)
+        {
+            if (WrongTypedUnits == 0) { UnitAccuracy = 100.0f; return; }
+            UnitAccuracy = WrongTypedUnits / CorrectTypedChars;
+        }
+
+    }
+}
