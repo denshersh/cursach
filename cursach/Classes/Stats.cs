20f9a634afb85f24021cea824cb474777654a266
DenShersh <georggnutiy@gmail.com>
2024-05-09 17:54:56 +0200

    Added a abstract Stats class with some workground

--- cursach/Classes/Stats.cs
+++ cursach/Classes/Stats.cs
@@ -8,5 +8,11 @@ namespace Classes
 {
     public abstract class Stats
     {
+        protected string StatUnitName;
+        // protected uint StatUnitCount;
+
+        public Stats(string statUnitName) {  StatUnitName = statUnitName; }
+
+        public abstract void CalculateStat(uint val);
     }
 }
