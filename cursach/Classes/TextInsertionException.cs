a0ca417ce1a1b955f3f9f0f7123a732342150ed4
DenShersh <georggnutiy@gmail.com>
2024-06-04 23:19:30 +0200

    Added custom exception class

---
+++ cursach/Classes/TextInsertionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class TextInsertionException : Exception
+    {
+        public TextInsertionException() { }
+
+        public TextInsertionException(string message) : base(message) { }
+
+        public TextInsertionException(string message, Exception inner) : base(message, inner) { }
+    }
+}
